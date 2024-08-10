d9a102e2af84f6d44cb5ebf492c1d2e934aba8b2
ACS <1692882051@qq.com>
2024-08-10 18:00:42 +0800

    reset to 323d50a fix:lengh undfine bug

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
     'standard',
-    'plugin:vue/vue3-essential'
+    'plugin:vue/vue3-essential',
+    'plugin:prettier/recommended',
   ],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: 'script',
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue'],
   rules: {
-  }
+    'vue/no-unused-components': 'warn',
+    'vue/no-unused-vars': 'warn',
+    'prettier/prettier': 'warn',
+  },
 }
---
+++ prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80, // 单行长度
+  tabWidth: 2, // 缩进长度
+  useTabs: false, // 使用空格代替tab缩进
+  semi: false, // 句末使用分号
+  singleQuote: true, // 使用单引号
+  quoteProps: 'as-needed', // 仅在必需时为对象的key添加引号
+  jsxSingleQuote: true, // jsx中使用单引号
+  trailingComma: 'all', // 多行时尽可能打印尾随逗号
+  endOfLine: 'auto', // 换行符使用lf
+  bracketSameLine: false, // 大括号不换行
+}
